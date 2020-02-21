760a426134a6d13ac697551e2e3522acb51db003
Alan69 <gameg1676@gmail.com>
2020-02-21 16:27:28 +0600

    first commi1t

---
+++ Assets/Scripts/Mountspawner.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mountspawner : MonoBehaviour
+{
+    private float maxTime = 1;
+    private float timer = 0;
+    private float height;
+    public GameObject pipe;
+
+    void Start()
+    {
+        height = Random.Range(1, 2);
+        maxTime = Random.Range(2,3.5f);
+        GameObject newpipe = Instantiate(pipe);
+        newpipe.transform.position = transform.position + new Vector3(0, Random.Range(-height, height), 0);
+    }
+    
+    void Update()
+    {
+        if(timer > maxTime)
+        {
+            height = Random.Range(1.5f, 2);
+            maxTime = Random.Range(3, 4.5f);
+            GameObject newpipe = Instantiate(pipe);
+            newpipe.transform.position = transform.position + new Vector3(0, Random.Range(-height, height), 0);
+            Destroy(newpipe, 10);
+            timer = 0;
+        }
+
+        timer += Time.deltaTime;
+        
+    }
+}
---
+++ Assets/Scripts/jumping.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jumping : MonoBehaviour
+{
+    public gameManager gameManager;
+    private Rigidbody2D rigidbody;
+    public float velocity = 1;
+    private Animator animator;
+    
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        //animator = GetComponent<Animator>();
+    }
+    
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            rigidbody.velocity = Vector2.up * velocity;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        gameManager.GameOver();
+    }
+}
